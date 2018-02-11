6cdbc0078c6256a1bdd3b98d5b57fff541b950bd
Esteban Castro <box.esteban@gmail.com>
2018-02-10 23:46:10 -0600

    indent code for better readability

--- content.php
+++ content.php
@@ -9,26 +9,21 @@ find_selected_page();
 ?>
 <div>
 	<!--TWO-->
-  <div id="navigation">
+	<div id="navigation">
 		<?php echo navigation($sel_subject, $sel_page); ?>
 		<br />
 		<a href="new_subject.php">+ Add a subject</a>
-  </div>
-
-  <div id="page">
+	</div>
+	<div id="page">
 		<?php if ($sel_subject) { ?>
-	    <h2>Manage Subject</h2>
-			Menu name: <?php echo $sel_subject["menu_name"]; ?><br />
-
-
+		<h2>Manage Subject</h2>
+		Menu name: <?php echo $sel_subject["menu_name"]; ?><br />
 		<?php } elseif ($sel_page) { ?>
-			<h2>Manage Page</h2>
-			Menu name: <?php echo $sel_page["menu_name"]; ?><br />
-
+		<h2>Manage Page</h2>
+		Menu name: <?php echo $sel_page["menu_name"]; ?><br />
 		<?php } else { ?>
-		 <h2 id="titulo">Welcome, Please Select a Subject</h2>
+		<h2 id="titulo">Welcome, Please Select a Subject</h2>
 		<?php }?>
-  </div>
+	</div>
 </div>
-
 <?php require("includes/footer.php"); ?>
